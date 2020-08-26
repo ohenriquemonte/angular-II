2257e4c80dc2420b9512faa8f43c9e7510d6a346
Henrique Montenegro <henrique.montenegro@slcagricola.com.br>
2020-08-26 19:02:28 -0300

    01.04 - Validacao de formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { HomeModule } from './home/home.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
@@ -6,19 +5,20 @@ import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { AppRoutingModule } from './app.routing.module';
 import { ErrorsModule } from './errors/errors.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    PhotosModule,
-    AppRoutingModule,
-    ErrorsModule,
-    HomeModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		PhotosModule,
+		AppRoutingModule,
+		ErrorsModule,
+		HomeModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,4 +1,3 @@
-import { SignInComponent } from './home/signin/signin.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -6,6 +5,7 @@ import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
+import { SignInComponent } from './home/signin/signin.component';
 
 const routes: Routes = [
     {
