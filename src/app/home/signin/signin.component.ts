f7254273ade2e71b2e83395f4bd39d74d233084e
Henrique Montenegro <henrique.montenegro@slcagricola.com.br>
2020-08-26 19:46:10 -0300

    01.07 - Enviando credenciais para a API

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { AuthService } from "src/app/core/auth.service";
 
 @Component({
     templateUrl: './signin.component.html'
@@ -9,15 +10,28 @@ export class SignInComponent implements OnInit {
     loginForm: FormGroup;
 
     constructor(
+        private authService: AuthService,
         private formBuilder: FormBuilder,
-    ) {
-
-    }
+    ) { }
 
     ngOnInit(): void {
         this.loginForm = this.formBuilder.group({
             userName: ['', Validators.required],
             password: ['', Validators.required],
         });
+    }
+
+    login() {
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+        this.authService
+            .authenticate(userName, password)
+            .subscribe(() => {
+                console.log(`autenticado`);
+            }, err => {
+                console.error(`erro ao autenticar ${JSON.stringify(err)}`);
+                this.loginForm.reset();
+                alert(`Invalid username or password`);
+            });
     }
 }
