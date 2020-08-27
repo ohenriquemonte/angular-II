a05086097a373b4b26ec09eba9bf82b39b6ec402
Henrique Montenegro <henrique.montenegro@slcagricola.com.br>
2020-08-27 01:02:44 -0300

    02.02 - Acesso do header de resposta

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 const API_URL = 'http://localhost:3000';
 
@@ -13,9 +14,14 @@ export class AuthService {
 	) { }
 
 	authenticate(userName: string, password: string) {
-		return this.http.post(`${API_URL}/user/login`, {
-			userName: userName,
-			password: password
-		});
+		return this.http
+			.post(`${API_URL}/user/login`, {
+				userName: userName,
+				password: password
+			}, { observe: 'response' })
+			.pipe(tap(res => {
+				const authToken = res.headers.get(`x-access-token`);
+				console.log(`authService authenticate() username ${JSON.stringify(userName)} - authToken ${JSON.stringify(authToken)}`);
+			}));
 	}
 }
