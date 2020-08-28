e34681e148221e31f1991b95e95003872ec7f516
Henrique Montenegro <henrique.montenegro@slcagricola.com.br>
2020-08-27 21:56:35 -0300

    02.02 - Separação de responsabilidades

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
-import { TokenService } from '../token/token.service';
+import { UserService } from '../user/user.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -11,7 +11,7 @@ const API_URL = 'http://localhost:3000';
 export class AuthService {
 
 	constructor(
-		private tokenService: TokenService,
+		private userService: UserService,
 		private http: HttpClient,
 	) { }
 
@@ -23,7 +23,7 @@ export class AuthService {
 			}, { observe: 'response' })
 			.pipe(tap(res => {
 				const authToken = res.headers.get(`x-access-token`);
-				this.tokenService.setToken(authToken);
+				this.userService.setToken(authToken);
 				console.log(`authService authenticate() username ${JSON.stringify(userName)} - authToken ${JSON.stringify(authToken)}`);
 			}));
 	}
---
+++ src/app/core/user/user.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@angular/core";
+import { TokenService } from "../token/token.service";
+import { Subject } from "rxjs";
+import { User } from "./user";
+import * as jwt_decode from 'jwt-decode';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+
+    private userSubject = new Subject<User>();
+
+    constructor(
+        private tokenService: TokenService,
+    ) {
+        this.tokenService.hasToken() &&
+            this.decodeAndNotify();
+    }
+
+    private decodeAndNotify() {
+        const token = this.tokenService.getToken();
+        const user = jwt_decode(token) as User;
+        this.userSubject.next(user);
+    }
+
+    getUser() {
+        return this.userSubject.asObservable();
+    }
+
+    setToken(token: string) {
+        this.tokenService.setToken(token);
+        this.decodeAndNotify();
+    }
+}
